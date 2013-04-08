0f1391c16eb94ee98a1f9a0b6e22a35ae6319a92
Søren Sønderby <tropehjelm@gmail.com>
2013-04-08 21:19:28 +0200

    remove nargout check from preallocate

--- NN/nnpreallocateloss.m
+++ NN/nnpreallocateloss.m
@@ -8,17 +8,17 @@ if ~isempty(nn.errfun)   %determine number of returned error values
         loss.val.e_errfun        = [];
     end
     
-    if nargout(nn.errfun) > 2
-        [~, ~, opts_out] = nn.errfun(nn, train_x(1,:), train_y(1,:));
-        for i = 1:opts.numepochs
-            loss.train.opts_out{i} = zeros(size(opts_out));
-            if opts.validation
-                loss.val.opts_out{i}   = zeros(size(opts_out));
-            else
-                loss.val.opts_out = {};
-            end
+    
+    [~, ~, opts_out] = nn.errfun(nn, train_x(1,:), train_y(1,:));
+    for i = 1:opts.numepochs
+        loss.train.opts_out{i} = zeros(size(opts_out));
+        if opts.validation
+            loss.val.opts_out{i}   = zeros(size(opts_out));
+        else
+            loss.val.opts_out = {};
         end
     end
+    
 else
     loss.val.e_errfun          = [];
     loss.train.e_errfun        = [];
