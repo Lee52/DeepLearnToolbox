6054f9de80a1f17c6668a07b9446cf513347947b
casperkaae <casperkaae@gmail.com>
2013-04-10 19:38:23 +0300

    Update cpLossToHost.m
    copy opts_out to host aswell

--- util/cpLossToHost.m
+++ util/cpLossToHost.m
@@ -1,10 +1,17 @@
 function [ hloss ] = cpLossToHost( dloss, opts )
 
+for i = 1:numel(dloss.train.opts_out)
+        hloss.train.opts_out{i} = gather(dloss.train.opts_out{i});
+end
+
 hloss.train.e       = gather(dloss.train.e);
 hloss.train.e_errfun  = gather(dloss.train.e_errfun);
 if opts.validation == 1
     hloss.val.e         = gather(dloss.val.e);
     hloss.val.e_errfun  = gather(dloss.val.e_errfun);
+    for i = 1:numel(dloss.train.opts_out)
+        hloss.val.opts_out{i} = gather(dloss.val.opts_out{i});
+    end
 end
 
 end
